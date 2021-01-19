a8fdc76c4c335ebd47b451d30e908494e86b2114
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-18 21:59:16 -0300

    Create Desafios.js

---
+++ Desafios.js
@@ -0,0 +1,14 @@
+const IMC = [
+    {
+        nome: 'carlos',
+        peso: 84,
+        altura: 1.88, 
+        sexo: 'masculino'
+    }
+]
+function calc (IMC) {
+    return (IMC[0].peso / (IMC[0].altura * IMC[0].altura))
+}
+if {
+    
+}
