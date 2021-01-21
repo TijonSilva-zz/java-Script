9eaef6c68ef1b652c192d58d9e4f7d0113474d89
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-21 09:47:18 -0300

    fluindo

---
+++ Dasaf06.js
@@ -0,0 +1,37 @@
+const usuarios = [
+    {
+        nome: 'Carlos',
+        tecno: ['HTML', 'CSS']
+    },
+    {
+        nome: 'Jasmine',
+        tecno: ['JavaScript', 'CSS']
+    },
+    {
+        nome: 'Tuane',
+        tecno: ['HTML', 'Node.js']
+    }
+]
+
+function CSS (usuarios) {
+    for (let i = 0; i<usuarios.length; i++) {
+        const usercss = checaSeUsuarioUsaCSS(usuario[i]) 
+    }
+}
+console.log (i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log (`${usuarios[0].nome} trabalha com ${usuarios[0].tecno}.`)
+//console.log (`${usuarios[1].nome} trabalha com ${usuarios[1].tecno}.`)
+//console.log (`${usuarios[2].nome} trabalha com ${usuarios[2].tecno}.`)
