259a8bdcca780290a858fed3716f90ea8284eb97
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-22 08:49:30 -0300

    to com um bug
    nao to conseguindo criar a function

--- desaf05.js
+++ desaf05.js
@@ -12,6 +12,14 @@ const usuarios = [
         tecno: ['HTML', 'Node.js']
     }
 ]
-console.log (`${usuarios[0].nome} trabalha com ${usuarios[0].tecno}.`)
-console.log (`${usuarios[1].nome} trabalha com ${usuarios[1].tecno}.`)
-console.log (`${usuarios[2].nome} trabalha com ${usuarios[2].tecno}.`)
+function usercss (usuarios) {
+    if (usuarios.length == 'CSS') true; {
+        console.log ('os usuarios que tem.')
+    } else {
+        console.log('ninguem trabalha com css')
+    } 
+}
+//console.log (`${usuarios[0].nome} trabalha com ${usuarios[0].tecno}.`)
+//console.log (`${usuarios[1].nome} trabalha com ${usuarios[1].tecno}.`)
+//console.log (`${usuarios[2].nome} trabalha com ${usuarios[2].tecno}.`)
+
