e854bc89c8ab0589477fb0f8b117ec42a538f12f
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-23 11:08:57 -0300

    ta indo

---
+++ desaf05.js
@@ -0,0 +1,40 @@
+const users = [
+    {
+        nome: 'Carlos',
+        tecno:  ['HTML', 'CSS']
+    },
+    {
+        nome: 'Jasmine',
+        tecno:  ['HTML', 'CSS']
+    },
+    {
+        nome: 'Tuane',
+        tecno: ['HTML','Node.js']
+    }
+]
+
+for (let i = 0; i < users.length;) {
+    console.log (`${users.nome} trabalha com ${users.tecno}`)
+}
+console.log (i)
+
+
+
+
+
+
+
+//function checausers1 (users) {
+  //  if (users[0].tecno == 'HTML','CSS') {
+    //    console.log (` ${users[0].nome} trabalha com ${users[0].tecno}`) 
+      //  } else {
+        //    console.log ('ninguem trabalha com CSS')
+//        //}
+//    }
+//checausers1 (users)
+//
+//function checausers2 (users) {
+//    if (users[1].tecno == 'HTML', 'CSS') {
+//        console.log (`${users[1].nome} trabalha com ${users[1].tecno}`)
+//    }
+//}
