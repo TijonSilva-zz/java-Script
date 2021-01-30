30d672c4e3e22089b701e5cbb29ba4af9f70828d
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-30 09:07:33 -0300

    ok

--- teste.js
+++ teste.js
@@ -12,9 +12,10 @@ const users = [
       tecno: ["HTML", "Node.js"],
     },
   ];
-  mengs(users);
+  
   function mengs(users) {
     for (let i = 0; i < users.length; i++) {
       console.log(`${users[i].nome} trabalha com ${users[i].tecno}`);
     }
   }
+  mengs(users)
