262ac1935925cead07b5398f2bfc4733f392ccd4
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-31 15:04:08 -0300

    tirei a funçaop q nao funfa

--- desaf05.js
+++ desaf05.js
@@ -19,17 +19,18 @@ function mengs (users) {
 }
 mengs (users)
 
-//function checausers1 (users) {
-  //  if (users[0].tecno == 'HTML','CSS') {
-    //    console.log (` ${users[0].nome} trabalha com ${users[0].tecno}`) 
-      //  } else {
-        //    console.log ('ninguem trabalha com CSS')
+
+// function checausers1 (users) {
+//    if (users[0].tecno == 'HTML','CSS') {
+//        console.log (` ${users[0].nome} trabalha com ${users[0].tecno}`) 
+//        } else {
+//            console.log ('ninguem trabalha com CSS')
 //        //}
 //    }
-//checausers1 (users)
-//
-//function checausers2 (users) {
+// checausers1 (users)
+
+// function checausers2 (users) {
 //    if (users[1].tecno == 'HTML', 'CSS') {
 //        console.log (`${users[1].nome} trabalha com ${users[1].tecno}`)
 //    }
-//}
+// }
