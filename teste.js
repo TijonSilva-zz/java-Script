766b34ed843e10c3fc6b9de4d2c266b661fe4524
Gabriel Tijon da Silva <gabrieltijon@icloud.com>
2021-01-31 17:49:46 -0300

    to tentando juntar duas funçoes

--- teste.js
+++ teste.js
@@ -1,38 +1,46 @@
 const users = [
-  {
-    nome: 'Gabriel'
-    nota: 10
-  },
-  {
-    
-  }
-]
-
-
-
-
-
-
-
-
-// const users = [
-//     {
-//       nome: "Carlos",
-//       tecno: ["HTML", "CSS"],
-//     },
-//     {
-//       nome: "Jasmine",
-//       tecno: ["HTML", "CSS"],
-//     },
-//     {
-//       nome: "Tuane",
-//       tecno: ["HTML", "Node.js"],
-//     },
-//   ];
+    {
+      nome: "Carlos",
+      tec1: "HTML", 
+      tec2: "CSS"
+    },
+    {
+      nome: "Jasmine",
+      tec1: "CSS",
+      tec2: "HTML"
+    },
+    {
+      nome: "Tuane",
+      tec1:  "HTML", 
+      tec2: "Node.js"
+    }
+  ];
   
-//   function mengs(users) {
-//     for (let i = 0; i < users.length; i++) {
-//       console.log(`${users[i].nome} trabalha com ${users[i].tecno}`);
-//     }
-//   }
-//   mengs(users)
+  function mengs(users) {
+    for (let i = 0; i < users.length; i++) {
+      console.log(`${users[i].nome} trabalha com ${users[i].tec1}, ${users[i].tec2}`);
+    }
+  }
+  mengs(users)
+
+  function checkusers1 (users) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].tec1 === 'CSS') {
+        console.log (true)
+      } else {
+        console.log (false)
+      }
+    }
+} 
+  checkusers1 (users) 
+  
+  function checkusers2 (users) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].tec2 == 'CSS') {
+        console.log (true)
+      } else {
+        console.log (false)
+      }
+    }
+} 
+  checkusers2 (users)         
