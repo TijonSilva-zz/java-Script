f4b8cc78264dcd44ef2658753c566bd9ed245dfb
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-05-25 22:08:59 -0300

    ula5

---
+++ aula05.js
@@ -0,0 +1,11 @@
+let pedidos = [
+ {id: 420, nome: 'Antonio', alimento: 'Sandubão de Salame', bebida: 'Suco de Limão'},
+ {id: 341, nome: 'Creuza', alimento: 'Risole', bebida: 'Refrigerante'},
+ {id: 433, nome: 'jasmin', alimento: 'Sandubão de Picanha', bebida: 'Suco de Tamarindo'},
+ {id: 42, nome: 'Feijão', alimento: 'Coxinha', bebida: 'Suco de Uva'},
+ {id: 890, nome: 'Gabriel', alimento: 'HotDog', bebida: 'Refrigerante'}
+
+]
+pedidos.filter(pedido =>{
+ return pedido.bebida == ('refrigerante')
+})
