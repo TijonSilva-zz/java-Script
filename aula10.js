c47e15c1161197bd6c6cf2c70775b202fe2c95fb
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-05-28 23:26:26 -0300

    teste alefy

--- aula10.js
+++ aula10.js
@@ -1,7 +1,17 @@
-const number =  [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 1, 2, 2, 5, 8, 8, 7]
- const string = [
-  'banana',
-   'maça',
-   'pera'
- ]
- console.log(number + string)  
+const empresa = {
+  nome = 'Rocketseat'
+},
+{
+  cor = 'roxo'
+},
+{
+  foco = 'Programaaçao'
+}
+
+ const endereço = {
+    rua = 'Rua Guilherme Gembala',
+    numero =  29
+  }
+
+
+console.log(`A empresa ${nome} se encontra na ${rua} numero ${numero}`)
